1543b09ddddc786c20cbe81cb91a3f74525f1797
thomasefthymiou78 <77837940+thomasefthymiou78@users.noreply.github.com>
2022-02-23 22:09:48 +0200

    Removing pandas dependency and some code refactoring

--- ecstats.py
+++ ecstats.py
@@ -4,14 +4,17 @@ import datetime
 import configparser
 import optparse
 import boto3
+import openpyxl
 
 # Metric Collection Period (in days)
 METRIC_COLLECTION_PERIOD_DAYS = 7
+
 SECONDS_IN_MINUTE = 60
-SECONDS_IN_HOUR = 3600
+SECONDS_IN_HOUR = 60 * SECONDS_IN_MINUTE
 SECONDS_IN_DAY = 24 * SECONDS_IN_HOUR
 
-
+RUNNING_INSTANCES_WORKSHEET_NAME = 'ClusterData'
+RESERVED_INSTANCES_WORKSHEET_NAME = 'ReservedData'
 
 def get_avg_metrics():
     metrics = [
@@ -28,38 +31,23 @@ def get_avg_metrics():
     ]
     return metrics
 
-
 def get_max_metrics():
     metrics = [
-        ('CurrItems', 'Maximum',
-         SECONDS_IN_DAY * METRIC_COLLECTION_PERIOD_DAYS),
-        ('BytesUsedForCache', 'Maximum',
-         SECONDS_IN_DAY * METRIC_COLLECTION_PERIOD_DAYS),
-        ('CacheHits', 'Maximum',
-         SECONDS_IN_DAY * METRIC_COLLECTION_PERIOD_DAYS),
-        ('CacheMisses', 'Maximum',
-         SECONDS_IN_DAY * METRIC_COLLECTION_PERIOD_DAYS),
-        ('CurrConnections', 'Maximum',
-         SECONDS_IN_DAY * METRIC_COLLECTION_PERIOD_DAYS),
-        ('NetworkBytesIn', 'Maximum',
-         SECONDS_IN_DAY * METRIC_COLLECTION_PERIOD_DAYS),
-        ('NetworkBytesOut', 'Maximum',
-         SECONDS_IN_DAY * METRIC_COLLECTION_PERIOD_DAYS),
-        ('NetworkPacketsIn', 'Maximum',
-         SECONDS_IN_DAY * METRIC_COLLECTION_PERIOD_DAYS),
-        ('NetworkPacketsOut', 'Maximum',
-         SECONDS_IN_DAY * METRIC_COLLECTION_PERIOD_DAYS),
-        ('EngineCPUUtilization', 'Maximum',
-         SECONDS_IN_DAY * METRIC_COLLECTION_PERIOD_DAYS),
-        ('Evictions', 'Maximum',
-         SECONDS_IN_DAY * METRIC_COLLECTION_PERIOD_DAYS),
-        ('ReplicationBytes', 'Maximum',
-         SECONDS_IN_DAY * METRIC_COLLECTION_PERIOD_DAYS),
-        ('ReplicationLag', 'Maximum',
-         SECONDS_IN_DAY * METRIC_COLLECTION_PERIOD_DAYS)
+        ('CurrItems', 'Maximum', SECONDS_IN_DAY * METRIC_COLLECTION_PERIOD_DAYS),
+        ('BytesUsedForCache', 'Maximum', SECONDS_IN_DAY * METRIC_COLLECTION_PERIOD_DAYS),
+        ('CacheHits', 'Maximum', SECONDS_IN_DAY * METRIC_COLLECTION_PERIOD_DAYS),
+        ('CacheMisses', 'Maximum', SECONDS_IN_DAY * METRIC_COLLECTION_PERIOD_DAYS),
+        ('CurrConnections', 'Maximum', SECONDS_IN_DAY * METRIC_COLLECTION_PERIOD_DAYS),
+        ('NetworkBytesIn', 'Maximum', SECONDS_IN_DAY * METRIC_COLLECTION_PERIOD_DAYS),
+        ('NetworkBytesOut', 'Maximum', SECONDS_IN_DAY * METRIC_COLLECTION_PERIOD_DAYS),
+        ('NetworkPacketsIn', 'Maximum', SECONDS_IN_DAY * METRIC_COLLECTION_PERIOD_DAYS),
+        ('NetworkPacketsOut', 'Maximum', SECONDS_IN_DAY * METRIC_COLLECTION_PERIOD_DAYS),
+        ('EngineCPUUtilization', 'Maximum', SECONDS_IN_DAY * METRIC_COLLECTION_PERIOD_DAYS),
+        ('Evictions', 'Maximum', SECONDS_IN_DAY * METRIC_COLLECTION_PERIOD_DAYS),
+        ('ReplicationBytes', 'Maximum', SECONDS_IN_DAY * METRIC_COLLECTION_PERIOD_DAYS),
+        ('ReplicationLag', 'Maximum', SECONDS_IN_DAY * METRIC_COLLECTION_PERIOD_DAYS)
     ]
     return metrics
-
 
 def calc_expiry_time(expiry):
     """Calculate the number of days until the reserved instance expires.
@@ -70,7 +58,6 @@ def calc_expiry_time(expiry):
         The number of days between the expiration date and now.
     """
     return (expiry.replace(tzinfo=None) - datetime.datetime.utcnow()).days
-
 
 def get_clusters_info(session):
     """Calculate the running/reserved instances in ElastiCache.
@@ -91,8 +78,7 @@ def get_clusters_info(session):
     # type, and name.
     for page in page_iterator:
         for instance in page['CacheClusters']:
-            if (instance['CacheClusterStatus'] == 'available' and
-               instance['Engine'] == 'redis'):
+            if (instance['CacheClusterStatus'] == 'available' and instance['Engine'] == 'redis'):
                 cluster_id = instance['CacheClusterId']
                 results['elc_running_instances'][cluster_id] = instance
 
@@ -102,21 +88,16 @@ def get_clusters_info(session):
     # Loop through active ElastiCache RIs and record their type and engine.
     for page in page_iterator:
         for reserved_instance in page['ReservedCacheNodes']:
-            if (reserved_instance['State'] == 'active' and
-               reserved_instance['ProductDescription'] == 'redis'):
+            if (reserved_instance['State'] == 'active' and reserved_instance['ProductDescription'] == 'redis'):
                 instance_type = reserved_instance['CacheNodeType']
                 # No end datetime is returned, so calculate from 'StartTime'
                 # (a `DateTime`) and 'Duration' in seconds (integer)
-                expiry_time = reserved_instance[
-                    'StartTime'] + datetime.timedelta(
-                    seconds=reserved_instance['Duration'])
+                expiry_time = reserved_instance['StartTime'] + datetime.timedelta(seconds=reserved_instance['Duration'])
                 results['elc_reserved_instances'][instance_type] = {
                     'count': reserved_instance['CacheNodeCount'],
                     'expiry_time': calc_expiry_time(expiry=expiry_time)
                 }
-
     return results
-
 
 def get_metric(cloud_watch, cluster_id, node, metric, aggregation, period):
     """Write node related metrics to file
@@ -143,23 +124,29 @@ def get_metric(cloud_watch, cluster_id, node, metric, aggregation, period):
     raw_data = [rec[aggregation] for rec in response['Datapoints']]
     return raw_data
 
-
-def create_data_frame():
-    """Create an empty dataframe with headers
+def create_workbook(outDir, section, region_name):
+    """Create an empty workbook dataframe with headers
     Args:
     Returns:
     The newely created pandas dataframe
     """
+    wb = openpyxl.Workbook()
+    ws = wb.active
+    ws.title = RUNNING_INSTANCES_WORKSHEET_NAME
+
     df_columns = ["ClusterId", "NodeId", "NodeType", "Region"]
     for metric, _, _ in get_max_metrics():
         df_columns.append(('%s (max over last week)' % metric))
     for metric, _, _ in get_avg_metrics():
         df_columns.append(('%s (peak last week / hour)' % metric))
-    df = pd.DataFrame(columns=df_columns)
-    return df
+    ws.append(df_columns)
 
+    ws = wb.create_sheet(RESERVED_INSTANCES_WORKSHEET_NAME)    
+    df_columns = ["Instance Type", "Count", "Remaining Time (days)"]
+    ws.append(df_columns)    
+    return wb
 
-def get_cluster_metrics(df, clusters_info, session):
+def get_running_instances_metrics(wb, clusters_info, session):
     """
     Get all the metrics for the clusters in the given set of clusters
     Args:
@@ -167,15 +154,13 @@ def get_cluster_metrics(df, clusters_info, session):
     Returns:
     """
     cloud_watch = session.client('cloudwatch')
-    row = []
-    i = 0
-
     running_instances = clusters_info['elc_running_instances']
+    ws = wb[RUNNING_INSTANCES_WORKSHEET_NAME]
+    row = []
+
     for instanceId, instanceDetails in running_instances.items():
         for node in instanceDetails.get('CacheNodes'):
-            print(
-                "Getting node % s details" %
-                (instanceDetails['CacheClusterId']))
+            print("Fetching node %s details" % (instanceDetails['CacheClusterId']))
             if 'ReplicationGroupId' in instanceDetails:
                 row.append("%s" % instanceDetails['ReplicationGroupId'])
             else:
@@ -191,7 +176,8 @@ def get_cluster_metrics(df, clusters_info, session):
                     node.get('CacheNodeId'),
                     metric,
                     aggregation,
-                    period)
+                    period
+                )
                 data_point = 0 if len(data_points) == 0 else data_points[0]
                 row.append(data_point)
             for (metric, aggregation, period) in get_avg_metrics():
@@ -201,42 +187,37 @@ def get_cluster_metrics(df, clusters_info, session):
                     node.get('CacheNodeId'),
                     metric,
                     aggregation,
-                    period)
+                    period
+                )
                 data_point = 0 if len(data_points) == 0 else max(data_points)
                 row.append(data_point)
-            df.loc[i] = row
+            ws.append(row)
             row = []
-            i += 1
+    return wb
 
-    df.sort_values(by=['NodeId'])
-
-
-def get_reserved_instances(clusters_info):
-    # create the dataframe
-    df_columns = ["Instance Type", "Count", "Remaining Time (days)"]
-    df = pd.DataFrame(columns=df_columns)
-
-    row = []
-    i = 0
-    reserved_instances = clusters_info['elc_reserved_instances']
+def get_reserved_instances_info(wb, clusters_info):
+    reserved_instances = clusters_info['elc_reserved_instances']        
+    ws = wb[RESERVED_INSTANCES_WORKSHEET_NAME]
     for instanceId, instanceDetails in reserved_instances.items():
-        row.append(("%s" % instanceId))
-        row.append(("%s" % instanceDetails['count']))
-        row.append(("%s," % instanceDetails['expiry_time']))
-        df.loc[i] = row
-        i = i + 1
-        row = []
-
-    return (df, i)
-
+        ws.append([
+            ("%s" % instanceId),
+            ("%s" % instanceDetails['count']),
+            ("%s," % instanceDetails['expiry_time'])
+        ])
+    return wb
 
 def process_aws_account(config, section, outDir):
     # connect to ElastiCache
     # aws key, secret and region
     aws_access_key_id = config.get(section, 'aws_access_key_id')
     aws_secret_access_key = config.get(section, 'aws_secret_access_key')
-    aws_session_token = config.get(section, 'aws_session_token')
     region_name = config.get(section, 'region_name')
+
+    if config.has_option(section, 'aws_session_token'):
+        aws_session_token = config.get(section, 'aws_session_token')
+    else:
+        aws_session_token = None
+
     session = boto3.Session(
         aws_access_key_id=aws_access_key_id,
         aws_secret_access_key=aws_secret_access_key,
@@ -244,20 +225,17 @@ def process_aws_account(config, section, outDir):
         region_name=region_name
     )
 
-    cluster_df = create_data_frame()
-
+    print(f"Requesting information for the {section} nodes")
     clusters_info = get_clusters_info(session)
 
-    get_cluster_metrics(cluster_df, clusters_info, session)
+    wb = create_workbook(outDir, section, region_name)
+    wb = get_running_instances_metrics(wb, clusters_info, session)
+    wb = get_reserved_instances_info(wb, clusters_info)
 
-    (reservedDF, _) = get_reserved_instances(clusters_info)
-
-    output_file_path = "%s/%s-%s.xlsx" % (outDir, section, region)
-    print(f"Writing {output_file_path}")
-    with pd.ExcelWriter(output_file_path, engine='xlsxwriter') as writer:
-        cluster_df.to_excel(writer, 'ClusterData')
-        reservedDF.to_excel(writer, 'ReservedData')
-
+    output_file_path = "%s/%s-%s.xlsx" % (outDir, section, region_name)
+    print(f"Writing output file {output_file_path}")
+    wb.save(output_file_path)
+    print("Done!")
 
 def main():
     parser = optparse.OptionParser()
@@ -283,8 +261,8 @@ def main():
     if not os.path.isdir(options.outDir):
         os.makedirs(options.outDir)
 
-    if options.configFile is None:
-        parser.print_help()
+    if not os.path.isfile(options.configFile):
+        print(f"Can't find the specified {options.configFile} configuration file")
         sys.exit(1)
 
     # Open and parse the configuration file.
@@ -295,9 +273,7 @@ def main():
     # will try to fetch the ElastiCache utilization by parsing the 
     # Cloudwatch statistics
     for section in config.sections():
-        print(section)
-        # process_aws_account(config, section, options.outDir)
-
+        process_aws_account(config, section, options.outDir)
 
 if __name__ == "__main__":
     main()
